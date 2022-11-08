5e3ce0227515921a619bd6b57324f69130c25f06
coscristian <crisquesadaco@gmail.com>
2022-11-07 19:41:02 -0500

    Drawer finished

---
+++ lib/pages/cash_close.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:ventas/widgets/drawer.dart';
+
+class CashClosePage extends StatelessWidget {
+  const CashClosePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cierre de caja"),
+      ),
+      drawer: const DrawerWidget(),
+    );
+  }
+}
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ventas/pages/cash_close.dart';
+import 'package:ventas/pages/payments.dart';
 
 class DrawerWidget extends StatelessWidget {
   const DrawerWidget({super.key});
@@ -8,33 +10,97 @@ class DrawerWidget extends StatelessWidget {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
-        children: const <Widget>[
+        children: <Widget>[
           DrawerHeader(
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               color: Colors.blue,
             ),
-            child: Text(
+            child: _header(),
+          ),
+          ListTile(
+            leading: const Icon(Icons.payment),
+            title: const Text('Cobros'),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const PaymentsPage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.shopping_bag),
+            title: const Text('Ventas'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.supervisor_account),
+            title: const Text('Clientes'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.money),
+            title: const Text('Cierre de caja'),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const CashClosePage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  } /*
+Text(
               'Drawer Header',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 24,
               ),
             ),
-          ),
-          ListTile(
-            leading: Icon(Icons.message),
-            title: Text('Messages'),
-          ),
-          ListTile(
-            leading: Icon(Icons.account_circle),
-            title: Text('Profile'),
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('Settings'),
-          ),
-        ],
-      ),
+            */
+
+  Widget _header() {
+    //TODO: Consultar los datos de la cabecera
+
+    const image = Icon(Icons.manage_accounts);
+    const name = "Cristian Quesada";
+    const email = "crisquesadaco@gmail.com";
+
+    return Row(
+      children: [
+        const CircleAvatar(
+          child: image,
+          radius: 30,
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              name,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(email,
+                style: TextStyle(
+                  color: Colors.white,
+                )),
+          ],
+        ),
+      ],
     );
   }
 }
